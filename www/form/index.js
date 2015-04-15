c1723a4410b47e8f0fc883dd3968d5a8cb5cc9a2
Vitaly Egorov <egorov@samsonos.com>
2015-04-15 15:14:33 +0300

    Fixing JS

--- www/form/index.js
+++ www/form/index.js
@@ -1,58 +1,67 @@
 /**
  * Created by egorov on 11.04.2015.
  */
-var loader = new Loader(s('body'));
 
-s(document).pageInit(function() {
-    initUserTable();
-    s('#createNewUser').tinyboxAjax({
-        html:'html',
-        renderedHandler: function(response, tb) {
-            s(".form2").ajaxSubmit(function(response) {
-                tb._close();
-                s('#content').html(response.list_html);
-                initUserTable();
-            });
-        },
-        beforeHandler: function() {
-            loader.show('Загрузка формы', true);
-            return true;
-        },
-        responseHandler: function() {
-            loader.hide();
-            return true;
-        }
-    });
-});
+// Init when in user module
+s('#user').pageInit(function(parent){
 
-function initUserTable() {
-    s('.btnEditUser').tinyboxAjax({
-        html:'html',
-        renderedHandler: function(response, tb) {
-            s(".form2").ajaxSubmit(function(response) {
-                loader.hide();
-                tb._close();
-                s('#content').html(response.list_html);
-                initUserTable();
-            }, function() {
-                loader.show(true);
+    // Creater generic loader instance
+    var loader = new Loader(s('body'));
+
+    // Content container
+    var container = s('#container', parent);
+
+    /**
+     * Initialize user form
+     * @param clickable Element to click fot form showing
+     */
+    var initForm = function(clickable) {
+        clickable.tinyboxAjax({
+            html: 'html',
+            renderedHandler: function (response, tb) {
+                s(".form2").ajaxSubmit(function (response) {
+                    loader.hide();
+                    tb._close();
+                    initList(response);
+                }, function () {
+                    loader.show(true);
+                    return true;
+                });
+            },
+            beforeHandler: function () {
+                loader.show('Загрузка формы', true);
                 return true;
-            });
-        },
-        beforeHandler: function() {
-            loader.show('Загрузка формы', true);
-            return true;
-        },
-        responseHandler: function() {
-            loader.hide();
-            return true;
-        }
-    });
+            },
+            responseHandler: function () {
+                loader.hide();
+                return true;
+            }
+        });
+    };
 
-    s('.btnDeleteUser').ajaxClick(function(response) {
-        s('#content').html(response.table);
-        initUserTable();
-    }, function() {
-        return confirm('Delete?');
-    });
-}
+    /**
+     * User module UI initialization and events
+     * @param response Server response for rendering
+     */
+    var initList = function(response)
+    {
+        // Render server response
+        if (response && response.list_html) {
+            container.html(response.list_html);
+        }
+
+        // Bind form list elements
+        initForm(s('.edit', container));
+
+        // Bind delete action
+        s('.delete', container).ajaxClick(initList, function(clicked) {
+            return confirm(clicked.a('title'));
+        });
+    };
+
+    // Bind form from sub-menu button
+    //initForm(s('#createNewUser'));
+
+    // Init interface
+    //initList();
+});
