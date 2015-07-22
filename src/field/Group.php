9aa6438f877e76bea4cdbae45ad03834426e3d4b
Vitaly Iegorov <egorov@samsonos.com>
2015-07-22 15:28:04 +0300

    Refactored and fixed correct ancestor form Group field

--- src/field/Group.php
+++ src/field/Group.php
@@ -4,10 +4,10 @@ namespace samsoncms\app\user\field;
 
 use samsonframework\core\RenderInterface;
 use samsonframework\orm\QueryInterface;
-use samsoncms\field\Generic;
+use samsoncms\form\field\Generic;
 
 /**
- * Overridden full name field
+ * Overridden group field
  * @package samsoncms\app\user
  */
 class Group extends Generic
@@ -15,31 +15,24 @@ class Group extends Generic
     /** @inheritdoc */
     public function render(RenderInterface $renderer, QueryInterface $query, $object)
     {
-        $groupSelect = '';
-        $groups = dbQuery('group')->cond('Active', 1)->exec();
-        foreach ($groups as $group) {
-            $groupSelect .= $group->GroupID.':'.$group->Name.',';
-        }
-        if (sizeof($groupSelect)) {
-            $groupSelect = substr($groupSelect, 0, -1);
+        // Get all available groups from db
+        $groupSelect = array();
+        foreach ($query->className('group')->cond('Active', 1)->exec() as $group) {
+            $groupSelect[] = $group->GroupID.':'.$group->Name;
         }
 
         // Set view
-        $renderer = $renderer->view($this->innerView);
-        $renderer->set(
-            m('samsoncms_input_application')->createFieldByType(
-                $query,
-                4,
-                $object,
-                $this->name
-            )->build($groupSelect),
-            'field'
-        );
-
-        // Render input field view
-        return $renderer
+        return $renderer->view($this->innerView)
             ->set('class', $this->css)
             ->set($object, 'item')
-            ->output();
+            ->set(
+                m('samsoncms_input_application')->createFieldByType(
+                    $query,
+                    4,
+                    $object,
+                    $this->name
+                )->build(implode(',', $groupSelect)),
+                'field'
+            )->output();
     }
 }
